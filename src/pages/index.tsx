8c7eaa387e423b6904e9f4792988d175cd5e1dba
Kaique Caires <kaique.caires@yahoo.com>
2021-10-18 14:36:30 -0300

    add stripe to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton'
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+type HomeProps = {
+  product: {
+    priceId: string
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -14,12 +23,30 @@ export default function Home() {
           <h1>New about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve(process.env.STRIPE_API_PRICE)
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }
